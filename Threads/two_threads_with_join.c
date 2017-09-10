b3e7c96eefbf599fab470c14a4339dbca87fcc43
krish-thorcode <krish.thorcode@gmail.com>
2017-09-10 13:01:20 +0530

    Added and debugged C program with two threads and parent waiting for the two threads to return.

--- Threads/two_threads_with_join.c
+++ Threads/two_threads_with_join.c
@@ -4,8 +4,8 @@
 
 typedef struct args
 {
-char *ch;
-unsigned int count;
+char ch;
+int count;
 }ARG;
 
 void *print_chars(void *parameters) //thread function. LINUX/UNIX thread functions return void* and accept void* parameters
@@ -14,7 +14,7 @@ ARG *paramet=(ARG*)parameters; //typecasting
 int i;
 
 for(i=0;i<paramet->count;i++)
-fprintf(paramet->ch,stderr);
+fputc(paramet->ch,stderr);
 
 return NULL;
 }
@@ -23,6 +23,23 @@ return NULL;
 int main(int argc,char **argv)
 {
 pthread_t *thread_1,*thread_2; //pthread_t specifically is a data type for thread id variables.
-ARG *arg_1,*arg_2;
+ARG *arg_1=(ARG*)malloc(sizeof(ARG));
+ARG *arg_2=(ARG*)malloc(sizeof(ARG));
 
-pthread_create(thread_1,NULL,&print_chars,arg_1
+arg_1->ch='x';
+arg_1->count=1000;
+
+arg_2->ch='o';
+arg_2->count=2000;
+
+pthread_create(thread_1,NULL,&print_chars,arg_1);
+pthread_create(thread_1,NULL,&print_chars,arg_2);
+
+puts("I'll wait for the other threads to return because if I exit, then the args I have passed to the threads will be out of scope for them.");
+
+pthread_join(*thread_1,NULL); //waiting for thread_1 to join
+pthread_join(*thread_2,NULL); //waiting for thread_2 to join
+
+return 0;
+
+}
