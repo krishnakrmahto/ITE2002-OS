a4a42ae9bb5945f53e4e6d87137716f653a84486
krish-thorcode <krish.thorcode@gmail.com>
2017-09-10 22:15:47 +0530

    main() function added and the code should run now.

--- Threads/critical_section_bank_transaction.c
+++ Threads/critical_section_bank_transaction.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
 #include<pthread.h>
 #include<stdlib.h>
-
+#include<sys/types.h>
 typedef struct args
 {
 int to_account;
+int to_account_balance;
 int from_account;
+int from_account_balance;
 float trans_amnt;
 }TRANSDETAILS;
 
-void* bank transaction(void *trans_det) // thread function for carrying out bank transactions
+void* bank_transaction(void *trans_det) // thread function for carrying out bank transactions
 {
 int default_canceltype; //it shall be used to store the default cancellation type.
 
@@ -19,13 +21,51 @@ return 1;
 
 /*since the transaction is about to be started, the following section should be invincible to cancellation attempts made by any other thread*/
 
-pthread_setcanceltype(PTHREAD_CANCEL_DISABLE,&default_canceltype)
+pthread_setcanceltype(PTHREAD_CANCEL_DISABLE,&default_canceltype); //setting cancellation for the transaction part disabled
 
 trans_details->to_account+=trans_details->trans_amnt;
-trans_details->from_account-=trans_details->trans_amnt;
+trans_details->from_accounte-=trans_details->trans_amnt;
 
-return 0;
+pthread_canceltype(defaul_canceltype,NULL); // resetting the cancellation type
+
+int return_val=0;
+return (void*)&return_val;
 }
 
+int main(int argc,char **argv)
+{
+TRANSDETAILS *transdets;
+pthread_t *thread_id;
+int trans_status;
 
+transdets=(TRANSDETAILS*)malloc(sizeof(TRANSDETAILS));
+puts("Enter source account number:");
+scanf("%d",&(transdets->from_account));
+puts("");
+puts("Enter source account balance: ");
+scanf("%d",&(transdets->from_account_balance));
+puts("");
+puts("Enter the destination account number:");
+scanf("%d",&(transdets->to_account));
+puts("");
+puts("Enter the destination account balance:");
+scanf("%d",&(transdets->to_account_balance));
+puts("");
+puts("Enter the amount to be transferred:");
+scanf("%d",&(transdets->trans_amnt));
 
+pthread_create(thread_id,NULL,&bank_transaction,transdets);
+
+puts("Transaction thread is going on...");
+
+pthread_join(trans_id,(void*)trans_status);
+
+if(trans_status==0) 
+
+else
+{
+puts("Source acc balance is not sufficient!");
+exit(1);
+}
+return 0;
+}
