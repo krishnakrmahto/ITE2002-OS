4c18d4c6748ca8a6a4c27706ec3dd12b45c154be
krish-thorcode <krish.thorcode@gmail.com>
2017-09-11 21:57:43 +0530

    Compilation is successful. Let's run it.

--- Threads/thread_specific_data_area.c
+++ Threads/thread_specific_data_area.c
@@ -7,7 +7,17 @@
 #include<pthread.h>
 #include<sys/types.h>
 
-void *write_log(void *thread_log_key_received)
+void write_log(pthread_key_t* thread_log_key,const char *log)
+{
+
+//getting the thread _specific value back
+
+FILE *fp=(FILE*)pthread_getspecific(*thread_log_key);
+fprintf(fp,"%s\n",log);
+return;
+}
+
+void *log_file_open(void *thread_log_key_received)
 {
 pthread_key_t* thread_log_key=(pthread_key_t*)thread_log_key_received;
 
@@ -15,17 +25,38 @@ FILE *fp;
 char *log_file_name;
 
 sprintf(log_file_name,"thread%d",(int)pthread_self());
+fp=fopen(log_file_name,"w");
+/* now what I would like to do is, set this file-pointer fp as a specific value for any thread that runs this function, so that each thread will have its own copy of log file pointer. This will enable us to store log file for each thread separately. */
+
+pthread_setspecific(*thread_log_key,(void*)fp); //pthread_setspecific()'s second argument (ie the thread specific value) should be void*
+write_log(thread_log_key,"The thread has started.");
+
+/* now thread will continue working and based on that log file can be written using the write_log function. */
+
+return NULL;
+}
+
+void clean_up_func(void *fp_rcv) //clean up also receives void* parameters
+{
+fclose((FILE*)fp_rcv);
+}
+
 
 int main()
 {
+int i;
 pthread_t thread_id[5];
 
 /* under this key, log of each thread will be saved which will be exclusively and independently*/
 pthread_key_t thread_log_key;
 
-pthread_key_create(&thread_log_key,clean_up_func) // clean up function will close the FILE opened.
+pthread_key_create(&thread_log_key,clean_up_func); // clean up function will close the FILE opened
 
+for(i=0;i<5;i++)
+pthread_create(&thread_id[i],NULL,log_file_open,(void*)&thread_log_key);
 
-pthread_create(&thread_id[i],NULL,write_log,(void*)&thread_log_key);
+for(i=0;i<5;i++)
+pthread_join(thread_id[i],NULL);
 
-
+return 0;
+}
