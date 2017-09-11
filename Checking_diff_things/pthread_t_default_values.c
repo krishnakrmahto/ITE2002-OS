72b1a6bb67bdb72bed843513dbb208bb1779c822
krish-thorcode <krish.thorcode@gmail.com>
2017-09-11 22:25:28 +0530

    initialising pthread_key_t value with malloc makes it zero. however not initialising pthread_t w/o initialising

--- Checking_diff_things/pthread_t_default_values.c
+++ Checking_diff_things/pthread_t_default_values.c
@@ -5,7 +5,7 @@
 int main()
 {
 pthread_t thread_id;
-pthread_key_t thread_key;
+pthread_key_t thread_key=*((pthread_key_t*)malloc(sizeof(pthread_key_t)));
 
 printf("%d\n%d\n",(int)thread_id,(int)thread_key);
 
