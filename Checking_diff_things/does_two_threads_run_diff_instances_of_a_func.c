01b5d3343b2f017dc457c07c52abab72d519470b
krish-thorcode <krish.thorcode@gmail.com>
2017-09-11 19:20:29 +0530

    Typo resolved.

--- Checking_diff_things/does_two_threads_run_diff_instances_of_a_func.c
+++ Checking_diff_things/does_two_threads_run_diff_instances_of_a_func.c
@@ -19,8 +19,8 @@ int arg=3;
 pthread_create(&thread1_id,NULL,thread_func,(void*)&arg);
 pthread_create(&thread2_id,NULL,thread_func,(void*)&arg);
 
-pthread_join(thread_1,NULL);
-pthread_join(thread_2,NULL);
+pthread_join(thread1_id,NULL);
+pthread_join(thread_2_id,NULL);
 
 return 0;
 }
