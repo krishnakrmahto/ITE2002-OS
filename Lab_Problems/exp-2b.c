eb2da7097170ca41b3bb794bfe2f055f443c4452
krish-thorcode <krish.thorcode@gmail.com>
2017-09-15 22:02:31 +0530

    Initial commit.

--- Lab_Problems/exp-2b.c
+++ Lab_Problems/exp-2b.c
@@ -2,9 +2,39 @@
 
 #include<stdio.h>
 #include<time.h> // for time-related functions
-#include<sys/types.h> // for size_t,open sys call
+#include<sys/types.h> // for size_t,mode_t,open sys call
 #include<fcntl.h> // for open system call
 #include<unistd.h> // for write, close system calls
 #include<sys/stat.h> // for open system call
 
-char *
+void open_file(char *filepath,int *fd_ptr,mode_t mode)
+{
+*fd_ptr=open(filename,O_WRONLY|O_APPEND,mode);
+
+if (*fd_ptr==-1) // file opening has failed
+{
+perror("open");
+exit(0);
+}
+}
+
+int main(int argc,char **argv)
+{
+char *filepath=*(argv[1]);
+int fd=0;
+mode_t mode=S_IWGRP | S_IRGRP | S_IRUSR | S_IWUSR | S_IROTH | S_IWOTH;
+
+open_file(filepath,&fd,mode);
+
+char *time_buffer;
+
+time_t time_sec=time(NULL); // if instead of NULL, some other time_t* var was passed then the returned secs would be saved there also.
+
+time_buffer=asctime(localtime(time_sec));
+
+write(fd,time_buffer,sizeof(time_buffer));
+
+close(fd);
+
+return 0;
+}
