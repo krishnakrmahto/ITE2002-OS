468c192f0c2b11e7ee7a1eafb5a644aadf63886a
krish-thorcode <krish.thorcode@gmail.com>
2017-09-26 11:26:43 +0530

    Sexy error! Error is, although different values of 'i' are being passed to the thread_func, within the thread func, 0 was being printed whenever philosopher's num was attempted to be  printed.

--- Lab_Problems/Exp-6/dining_philosopher.c
+++ Lab_Problems/Exp-6/dining_philosopher.c
@@ -4,24 +4,27 @@
 #include<unistd.h>
 
 pthread_mutex_t mutex_fork[5]; // 5 mutexes for each fork
+pthread_key_t phil_num;
 
 void *philosopher_func(void *arg)
 {
-int* phil_no=(int*)(arg);
-printf("Philosopher %d is thinking.\n",*phil_no);
+printf("%d\n",*((int*)arg));
+pthread_setspecific(phil_num,arg);
+
+printf("Philosopher %d is thinking.\n",*(int*)pthread_getspecific(phil_num));
 sleep(3);
 
 /*philosopher number phi_no will now pick up two forks.*/
-pthread_mutex_lock(&mutex_fork[*phil_no]);
-pthread_mutex_lock(&mutex_fork[(*phil_no+1)%5]);
+pthread_mutex_lock(&mutex_fork[*(int*)pthread_getspecific(phil_num)]);
+pthread_mutex_lock(&mutex_fork[(*(int*)pthread_getspecific(phil_num)+1)%5]);
 
-printf("Philosopher %d is eating.\n",*phil_no);
+printf("Philosopher %d is eating.\n",*(int*)pthread_getspecific(phil_num));
 sleep(2);
 
-pthread_mutex_unlock(&mutex_fork[*phil_no]);
-pthread_mutex_unlock(&mutex_fork[(*phil_no+1)%5]);
+pthread_mutex_unlock(&mutex_fork[*(int*)pthread_getspecific(phil_num)]);
+pthread_mutex_unlock(&mutex_fork[(*(int*)pthread_getspecific(phil_num)+1)%5]);
 
-printf("Philosopher %d finished eating.\n",*phil_no);
+printf("Philosopher %d finished eating.\n",*(int*)pthread_getspecific(phil_num));
 
 return NULL;
 }
@@ -29,13 +32,17 @@ return NULL;
 int main(int argc,char **argv)
 {
 pthread_t thread_philosopher[5];
+pthread_key_create(&phil_num,NULL);
+
 int i;
 for(i=0;i<5;i++)
 pthread_mutex_init(&mutex_fork[i],NULL);
 
 for(i=0;i<5;i++)
+{
+//printf("%d\n",i);
 pthread_create(&thread_philosopher[i],NULL,philosopher_func,(void*)&i);
-
+}
 for(i=0;i<5;i++)
 pthread_mutex_destroy(&mutex_fork[i]);// freeing the space occupied by mutex
 
