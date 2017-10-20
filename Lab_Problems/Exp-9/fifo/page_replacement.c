162427cb7af99491c99f5c0870641718b72ff601
krish-thorcode <krish.thorcode@gmail.com>
2017-10-20 15:09:25 +0530

    completed opt

--- Lab_Problems/Exp-9/fifo/page_replacement.c
+++ Lab_Problems/Exp-9/fifo/page_replacement.c
@@ -41,7 +41,7 @@ void fifo(int pages[],int n, int fr)
 
 void lru(int pages[],int n,int fr)
 {
-	int frames[MAX] = {0}, min=MAX, present=0, i, j, k, k_min = 0, new_start; //min = MAX instd of MAX choose any big number for line 54 to satsfy for the first time
+	int frames[MAX] = {0}, min, present=0, i, j, k, k_min = 0, new_start;
 
 	for(k=0,i=0;k<fr;k++,i++)
 	{
@@ -97,7 +97,65 @@ void lru(int pages[],int n,int fr)
 
 void opt(int pages[],int n, int fr)
 {
-	puts("hahaha");
+	int frames[MAX] = {0}, max=0, present=0, i, j, k, k_max = 0, new_start, unused_in_future;
+
+	for(k=0,i=0;k<fr;k++,i++)
+	{
+		frames[k] = pages[i];
+		puts("Page miss");
+		print_frames(frames,fr);
+	}
+	
+	//remain_page = n-fr;
+
+	new_start = k;
+
+	for(i=new_start;i<n;i++)
+	{
+		max = 0; //for each page[i] reset min (holds the page index of lr element)
+		present = 0;
+		for(k=0;k<fr;k++)
+		{
+			if(pages[i]==frames[k])
+			{
+				present = 1;
+				puts("Page hit");
+				break;
+			}
+		}
+		
+		if(present == 0)	
+		{
+			puts("Page miss");
+				for(k=0;k<fr;k++) // for each frame..
+				{
+					unused_in_future = 1; //for frame (to check if unused in future)
+					for(j=i+1;j<n;j++) // ..for each page after current page being allocated..
+					{
+						if(frames[k] == pages[j]) // ..check if frame will be used afterwards by comparing with each pages[j] ..
+						{
+							unused_in_future = 0;
+							if(j>=max)
+							{
+								max = j;
+								k_max = k;
+							}
+							break;
+						}
+					}
+					if(unused_in_future == 1) // if frame unused in future, stop comapring other frames...
+					{
+						k_max = k;
+						break;
+					}
+				}
+				frames[k_max] = pages[i];
+				print_frames(frames,fr);
+	
+		}
+		else
+			print_frames(frames,fr);
+	}
 }
 
 int main(int argc,char *argv[])
