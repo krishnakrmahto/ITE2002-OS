a39c3931193ab7a160432ae6b6903833cd54039c
krish-thorcode <krish.thorcode@gmail.com>
2017-10-21 12:10:47 +0530

    simply commiting

--- Lab_Problems/Exp-10/file_allocation_algo.c
+++ Lab_Problems/Exp-10/file_allocation_algo.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-// 1 disk unit each of MAX_DISK is equal to 512 Bytes
+// 1 disk unit each of MAX_DISK_DISK is equal to 512 Bytes
 #define MAX_DISK 5
 #define occupied 1
 #define free 0
 
 
 // 1 index location represents 1 block of disk = 512 Bytes
-int disk[MAX];
+int disk[MAX_DISK];
+int file_size = 2;
 
 
 void contiguous()
 {
-	int i,flag[MAX];
+	int i,flag[MAX_DISK];
 
-	for(i=0;i<MAX && ;i++)
+	for(i=0;i<MAX_DISK;i++)
 	{
 		if(disk[i] == free)
 		{
@@ -36,6 +37,58 @@ void contiguous()
 	}
 }
 
+void indexed()
+{
+	int file_index[2], i, j=0,limit = file_size;
+
+	for(i=0;i<MAX_DISK && limit;i++)
+	{
+		if(disk[i] == free)
+		{
+			disk[i] = occupied;
+			file_index[j++] = i;
+			limit--;
+		}
+	}
+	if(j==2)
+	{
+		puts("File allocation successful");
+		printf("Following is the status of the file index:\nfile 1: %d\nfile 2: %d\n",file_index[0],file_index[1]);
+		return ;
+	}
+	else
+	{
+		puts("Indexed allocation not possible.");
+		return ;
+	}
+
+
+}
+
+void linked()
+{
+	typedef struct linked_disk
+	{
+		int space;
+		struct linked_disk* next;
+	}LD;
+
+	LD *head, *new;
+
+	head = (LD*)malloc(sizeof(LD));
+	head->space = occupied;
+	head->next = NULL;
+
+	new = (LD*)malloc(sizeof(LD));
+	new->space = occupied;
+	new->next = NULL;
+	head->next = new;
+
+	LD *temp;
+
+	puts("File allocation completed.");
+}
+
 int main(int argc, char* argv[])
 {
 	int choice,i,j;
@@ -46,12 +99,12 @@ int main(int argc, char* argv[])
 	switch(choice)
 	{
 		case 1:
-		for(i=0;i<MAX;i++)
+		for(i=0;i<MAX_DISK;i++)
 			disk[i] = free;
 		break;
 
 		case 2:
-		for(i=0;i<MAX;i++)
+		for(i=0;i<MAX_DISK;i++)
 		{
 			if(i%2 == 0)
 				disk[i] = occupied;
@@ -60,21 +113,21 @@ int main(int argc, char* argv[])
 		}
 
 	}
-
-	puts("");
-
-	puts("Select allocation type:\n1. Contiguous\n2. Indexed\n3. Linked\n4. Exit");
-	scanf("%d",&choice);
-
+	
 	do
 	{
+
+		puts("");
+
+		puts("Select allocation type:\n1. Contiguous\n2. Indexed\n3. Linked\n4. Exit");
+		scanf("%d",&choice);
 		switch(choice)
 		{
 			case 1:
 			contiguous();
 			break;
 
-			case 2;
+			case 2:
 			indexed();
 			break;
 
